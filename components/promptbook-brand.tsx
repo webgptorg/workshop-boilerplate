477539c47e77245f6e0a0e0cc57d90edf0aa6c5c
Pavol Hejný <me@pavolhejny.com>
2026-09-07 17:30:21 +0200

    The logo and the name of the project should lead to the homepage of the app

--- components/promptbook-brand.tsx
+++ components/promptbook-brand.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 export function PromptbookBrand() {
   return (
-    <a className="brand" href="https://www.ptbk.io/" aria-label="Promptbook homepage">
+    <Link className="brand" href="/" aria-label="Callbook homepage">
       <span className="brand-name">Promptbook</span>
       <span className="brand-label">Callbook</span>
-    </a>
+    </Link>
   );
 }
