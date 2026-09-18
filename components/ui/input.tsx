e5ed834dd1d37bb486d983237a927ba642b8aba9
Pavol Hejný <me@pavolhejny.com>
2026-09-18 14:17:23 +0200

    Revert "First shot of the game #3"
    This reverts commit b284b1a36a059d3c31249df410e6cf0ecff67617.

--- components/ui/input.tsx
+++
@@ -1,5 +0,0 @@
-import type { InputHTMLAttributes } from "react";
-
-export function Input(props: InputHTMLAttributes<HTMLInputElement>) {
-  return <input {...props} />;
-}
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,1 +1,11 @@
-export default [{ ignores: [".next/**", "node_modules/**"] }];
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+];
+
+export default eslintConfig;
