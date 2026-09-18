b284b1a36a059d3c31249df410e6cf0ecff67617
Pavol Hejný <me@pavolhejny.com>
2026-09-18 14:17:17 +0200

    First shot of the game #3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,1 @@
-import { FlatCompat } from "@eslint/eslintrc";
-
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default [{ ignores: [".next/**", "node_modules/**"] }];
